ec2a028dfdfa768b5c08eb73aa9a0c66b0b27f9d
JBears-Springpoint <jbears@springpointstructural.com>
2025-03-14 09:28:53 -0400

    Added cases sorting to openre module

--- src/structuraltools/io/openre.py
+++ src/structuraltools/io/openre.py
@@ -35,6 +35,7 @@ class Model:
         self.nodes = self.model[0][0]
         self.node_reactions = self.model[2][2]
 
+        # Get the units from the OpenRE file
         unit_codes = self.model.attrib["Units"].split("-")
         match unit_codes[0]:
             case "Kip":
@@ -51,18 +52,41 @@ class Model:
             case _:
                 raise NotImplementedError(f"{unit_codes} units are not currently supported")
 
-    def get_node_reactions(self, node: str) -> pd.DataFrame:
+        # Sort and store the load conditions
+        self.load_cases = {}
+        self.design_combinations = set()
+        self.service_combinations = set()
+        for load_condition in self.model[0][4]:
+            if load_condition.get("Type") == "Design":
+                self.design_combinations.add(load_condition.get("ID"))
+            elif load_condition.get("Type") == "Service":
+                self.service_combinations.add(load_condition.get("ID"))
+            else:
+                self.load_cases.update(
+                    {load_condition.get("ID"): load_condition.get("Category")})
+
+    def get_node_reactions(self, node: int, cases: str) -> pd.DataFrame:
         """Return the reaction forces for the specified node
 
         Parameters
         ==========
 
         node : str
-            ID number of the node"""
+            ID number of the node
+
+        cases : str
+            One of: "load_cases", "design_combinations", or "service_combinations"
+            indicating which load cases/combinations to return the reactions for"""
+        node = str(node)
+        if cases == "load_cases":
+            cases = self.load_cases.keys()
+        else:
+            cases = getattr(self, cases)
         reactions = pd.DataFrame(columns=("FX", "FY", "FZ", "MX", "MY", "MZ"))
         for entry in self.node_reactions:
-            node_id, case_id = entry.attrib.values()
-            if node_id == node:
+            node_id = entry.get("NodeID")
+            case_id = entry.get("LoadCombinationID", entry.get("LoadCaseID"))
+            if node_id == node and case_id in cases:
                 reactions.loc[case_id, :] = 0
                 for reaction in entry:
                     reactions.at[case_id, reaction.tag] = float(reaction.text)
